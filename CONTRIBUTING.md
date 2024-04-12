40bd8da7224287c8a4824251125337dec760d073
kolarorz <kolarorz@gmail.com>
2024-04-12 19:01:07 +0800

    feat: 更新文档

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,13 @@
 # 贡献代码
 
+1. fork 代码，
+
+2. git clone 本地
+
+3. 修改代码
+
+4. 提交pr
+
 ## 本地运行
 
 ```shell
@@ -10,4 +18,12 @@ pnpm i
 npm run dev
 ```
 
-## 发布
+## docs
+
+master分支：github actions自动发布文档
+
+## publish
+
+1. 更改版本号
+2. 添加release和tag
+3. github actions自动发布组件
--- docs/examples/huge-data/Main.vue
+++ docs/examples/huge-data/Main.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="main">
-    <button class="demo-btn" @click="onLoadData">解压30w数据</button>
+    <Operate
+      :virtListRef="virtListRef"
+      :length="list.length"
+      :visible.sync="visible"
+    ></Operate>
 
     <div style="padding: 10px 0">
+      <button class="demo-btn" @click="onLoadData" style="color: red">
+        解压30w数据
+      </button>
+      <span>&nbsp;&nbsp;&nbsp;</span>
       <span>Total: {{ list.length }} </span>
       <span>&nbsp;&nbsp;&nbsp;</span>
       <span>RenderBegin: {{ reactiveData?.renderBegin }} </span>
@@ -45,9 +53,13 @@ import JSZip from 'jszip';
 import { VirtList } from 'vue-virt-list';
 import Item from './Item.vue';
 import url from '../data/30w.zip?url';
+import Operate from '../components/OperateGroup.vue';
 
+const visible = ref(false);
 const loading = ref(false);
-const virtListRef: Ref<InstanceType<typeof VirtList> | null> = ref(null);
+const virtListRef: Ref<typeof VirtList | null> = ref(null);
+
+// 巨量数据请使用 `ShallowRef !!!`，`否则内存就爆了!!!` 如需响应式，请使用 `virtListRef.value?.forceUpdate();`。或者，自己新增一个`renderKey`来控制渲染
 const list: ShallowRef<any[]> = shallowRef([]);
 
 const reactiveData = computed(() => {
@@ -59,6 +71,8 @@ onMounted(() => {
 });
 
 async function onLoadData() {
+  if (list.value.length !== 0) return;
+
   loading.value = true;
   fetch(url)
     .then((res) => {
